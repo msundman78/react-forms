7f32cae223aaebfa2e4939be82fb4179564a451d
Mathias Sundman <mathias.sundman@it-total.se>
2021-03-17 00:09:57 +0100

    Added CreateActivity dialog

--- src/Pages/Activities/Activities.js
+++ src/Pages/Activities/Activities.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import {useState} from 'react';
+
 import './Activities.css';
 import CreateActivity from './CreateActivity';
 
 
 const listActivities = [
-  {id:1, time: "2021-03-15 10:30", name: "WOD", gym: "Grottan", booked: 3},
-  {id:2, time: "2021-03-16 10:30", name: "WOD", gym: "Grottan", booked: 4},
-  {id:3, time: "2021-03-17 10:30", name: "WOD", gym: "Utegymmet", booked: 5}  
+  {time: "2021-03-15 10:30", name: "WOD", gym: "Grottan", booked: 3},
+  {time: "2021-03-16 10:30", name: "WOD", gym: "Grottan", booked: 4},
+  {time: "2021-03-17 10:30", name: "WOD", gym: "Utegymmet", booked: 5}  
 ];
 
 const Activities = () => {
-  const act = listActivities.map((a) => (
-    <tr key={a.id}>
+  const [actState, setActState] = useState(listActivities);
+
+  const addActivity = (data) => {
+    console.log("Add Act");
+    const NewList = [...actState, data
+//      {time: "2021-03-18 10:30", name: "WOD", gym: "Hemma", booked: 5}
+    ];
+    setActState(NewList);
+  }
+  const act = actState.map((a, index) => (
+    <tr key={index}>
       <td>{a.time}</td>
       <td>{a.name}</td>
       <td>{a.gym}</td>
@@ -35,7 +46,7 @@ const Activities = () => {
           </tbody>
         </table>
         <br />
-        <CreateActivity />
+        <CreateActivity addActivity={addActivity}/>
     </div>
   );
 }
--- src/Pages/Activities/CreateActivity.js
+++ src/Pages/Activities/CreateActivity.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,8 +6,14 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-export default function CreateActivity() {
-  const [open, setOpen] = React.useState(false);
+export default function CreateActivity({addActivity}) {
+  const [open, setOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    gym: '',
+    desc: '',
+    time: ''
+  });
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -17,6 +23,17 @@ export default function CreateActivity() {
     setOpen(false);
   };
 
+  const handleCreate = (data) => {
+    addActivity(formData);
+    setOpen(false);
+  };
+
+  const updateFormData = (e) => {
+    const newFormData = Object.assign({}, formData);
+    newFormData[e.target.id] = e.target.value;
+    setFormData(newFormData);
+  }
+
   return (
     <div>
       <Button variant="contained" color="primary" onClick={handleClickOpen}>
@@ -25,14 +42,14 @@ export default function CreateActivity() {
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Create Activity</DialogTitle>
         <DialogContent>
-          <TextField autoFocus margin="dense" id="gym" label="Gym Name" fullWidth />
-          <TextField margin="dense" id="name" label="Activity Name" fullWidth />
-          <TextField margin="dense" id="name" label="Description" multiline rows={4} fullWidth />
-          <TextField margin="dense" id="date" label="Datum" fullWidth />
+          <TextField margin="dense" id="gym" label="Gym Name" value={formData.gym} onChange={updateFormData} fullWidth autoFocus/>
+          <TextField margin="dense" id="name" label="Activity Name" value={formData.name} onChange={updateFormData} fullWidth />
+          <TextField margin="dense" id="desc" label="Description" value={formData.desc} onChange={updateFormData}multiline rows={4} fullWidth />
+          <TextField margin="dense" id="time" label="Datum" value={formData.time} onChange={updateFormData} fullWidth />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">Cancel</Button>
-          <Button onClick={handleClose} color="primary">Create</Button>
+          <Button onClick={handleCreate} color="primary">Create</Button>
         </DialogActions>
       </Dialog>
     </div>
